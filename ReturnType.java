b203fe52c1749be08ac7d6686ad76c79b65054db
Jakiya-18 <jakiyabanu35@gmail.com>
2024-11-12 20:13:54 +0530

    Returns type file

--- ReturnType.java
+++ ReturnType.java
@@ -6,6 +6,8 @@ public class ReturnType{
         int result = compareNumber(5 , 10);
         System.out.println("The greatest number in x and y is : " + result);
 
+        instagramId("JAKIYA", 21 , 'F', 38.99d);
+
     }
 
     public static String myName(String name){
@@ -20,5 +22,18 @@ public class ReturnType{
         else 
         return y;
 
+    }
+
+    public static boolean instagramId(String name, int age, char gender, double value){
+        if (name!= null){
+            if (age > 18){
+                 System.out.println("NAME :" +name+ "," + "AGE :" +age+ "," + "GENDER :" +gender+ "," + "USER'S IN KARNATAKA :" +value);
+                 return true;
+            }
+            System.out.println("Age is invalid");
+            return false;
+        }
+        System.out.println("Plz enter the name");
+        return false;
     }
 }
