b57a4771a9d1a8378e5651b523b352f545a4fe2b
Jakiya-18 <jakiyabanu35@gmail.com>
2025-04-01 20:30:12 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,148 @@
+1. Variable:- variable means changing the value
+-> varying the value
+->it is a container which Stores the value.
+
+Ex :-   int salary = 25000;
+ here, int is a one of the primitive data type and salary is a instance which can be changed and, 25000 -> initializing the value.
+
+2. int salary = 25000;
+   char section = 'A';
+   boolean isItAvailable = true;
+   float cgpa = 8.01f;
+   double sgpa = 7.55d;
+   long phoneNo = 9972652838l;
+   short pinCode = 583227;
+
+3.     primitive data type                Non primitive data type
+
+ 1. it has 7 data types               1. it has 3 data types
+
+ 2. size depends on the data type     2. same size
+
+ 3. starts with lowercase letters     3. starts with uppercase letter.
+
+
+
+4.  10.000
+
+5. null
+
+
+METHODS AND RETURN TYPE 
+
+1. Method :- method is a block of code, which executes whenever we call.
+   Purpose:- Reusability of code.
+
+Ex :- 
+public class Car{
+
+public static void main(String [] args){
+   movingCar();
+}
+
+public static void movingCar(){
+System.out.println("Car is moving");
+}
+}
+
+2.  public class Add{
+
+public static void main(String [] args){
+  addedNumbers();
+}
+
+public static int addedNumbers(int num1, int num2){
+ num1 = 25;
+ num2 = 25;
+return addedNumbers;
+
+}
+}
+
+3. void method will return nothing, it will just complete the task
+   non void method will complete the task and also return the value as according to the      data type we passed.
+
+VOID EX :->   
+public class Person {
+ public static void main(String [] args){
+
+}
+
+public static void cleanFloor(){
+System.out.println(" floor is cleaning ");
+}
+}
+
+NON VOID EX :->
+
+public class Salary{
+public static void main(String [] args){
+System.out.println("Salary is credited...");
+System.out.println(isSalaryCredited());
+}
+
+public static boolean isSalaryCredited(){
+return true;
+}
+}
+
+4. 20
+
+
+4. Method overloading :- method overloading allows to have a multiple methods with a same name but different in parameters. 
+
+public class Person{
+
+public static void main(String [] args){
+}
+
+public static void add(int num1){
+System.out.println("number 1");
+}
+
+public static void add(int num1, num2){
+System.out.println("passing 2 parameters with having same method");
+}
+}
+
+
+5. it will show a compile time error, becoz the compiler is expecting a any of the data type return.
+
+
+
+PARAMETERS AND PASS BY VALUE CONCEPT;
+
+1. PARAMETRS :- parameters are variable used in method declaration to pass a arguments to method.
+
+4. no, becoz it only retrun one value.
+
+
+
+ARRAY
+
+1. arrays are fundamental structure in java, it allows to store multiple values of the same data type in a singe variable.
+
+ -> group of elements which are similar in type.
+
+normal varibale stores only single value, but array stores multiple values
+
+
+2. int [] age = {25,26,27,28,29,30,43};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"
